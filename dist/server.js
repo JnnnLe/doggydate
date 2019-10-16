4631e0968f4ea36f491565cf12c2a6574dfcf875
JnnnLe <jnnnle.sf@gmail.com>
2019-10-16 10:52:06 -0700

    Start Ecpress server and setup babel config files

---
+++ dist/server.js
@@ -0,0 +1,44 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.start = exports.app = void 0;
+
+var _express = _interopRequireDefault(require("express"));
+
+var _bodyParser = require("body-parser");
+
+var _morgan = _interopRequireDefault(require("morgan"));
+
+var _cors = _interopRequireDefault(require("cors"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+let port = 3000;
+const app = (0, _express.default)();
+exports.app = app;
+app.disable('x-powered-by');
+app.use((0, _cors.default)());
+app.use((0, _bodyParser.json)());
+app.use((0, _bodyParser.urlencoded)({
+  extended: true
+}));
+app.use((0, _morgan.default)('dev'));
+app.get('/data', (req, res) => {
+  res.json({
+    message: "Jennifer Lê Jowett's server is running!"
+  });
+});
+app.post('/data', (req, res) => {
+  console.log(req.body);
+  res.status(200).end();
+});
+
+const start = () => {
+  app.listen(port, () => {
+    console.log('Server on port 3000');
+  });
+};
+
+exports.start = start;
---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
+let port = 3000
+
+export const app = express()
+
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.get('/data', (req, res) => {
+  res.json({ message: "Jennifer Lê Jowett's server is running!" })
+})
+
+app.post('/data', (req, res) => {
+  console.log(req.body)
+  res.status(200).end()
+})
+
+export const start = () => { 
+  app.listen(port, () => {
+    console.log('Server on port 3000')
+  })
+}
