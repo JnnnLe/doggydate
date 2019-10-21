c5ce1e28404d48c33e135c8cb69ffb9634b2185b
JnnnLe <jnnnle.sf@gmail.com>
2019-10-21 16:39:01 -0700

    Connect to mongodb

--- src/resources/user/user.controllers.js
+++ src/resources/user/user.controllers.js
@@ -1,16 +1,17 @@
 // after models get started on controller
 import { User } from './user.models'
 import mongoose from 'mongoose'
-import { connect } from 
+import { connect } from '../../utils/db'
 
-const run = async ()=> {
+const run = async () => {
   await connect('mongodb://localhost:27017/api')
   const user = await User.create({
-    username: 'jnnnle', 
+    username: 'jnnnle',
     email: 'jennifer@email.com',
     password: 'password',
     location: 'Oakland'
   })
-
   console.log(user)
 }
+
+run()
