4ffa4f0357a0ad1e1cc1981b172199c34982e973
JnnnLe <jnnnle.sf@gmail.com>
2019-10-23 11:16:09 -0700

    Connect to Mongo db

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,12 +1,9 @@
-// TODO: establish connection to db
-import mongoose from 'mongoose'
-require('dotenv').config()
+// import mongoose from 'mongoose'
+const mongoose = require('mongoose')
+require('dotenv').config({ path: '../../.env' })
 
 let uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@doggydate-biwhc.gcp.mongodb.net/test?retryWrites=true&w=majority`
 
-mongoose.connect(uri, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on(`error`, console.error.bind(console, `Connection error with MongoDB:`))
-db.once(`open`, () => {
-  console.log(`Connected to MongoDB!`)
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+  err ? console.log(err) : console.log('Success, connected to Mongo database :).')
 })
