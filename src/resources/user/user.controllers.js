fafb8fdc2447fdc74c3cddb525d8682bff683a3c
JnnnLe <jnnnle.sf@gmail.com>
2019-10-23 15:42:00 -0700

    Debate removing controllers, outdated model

--- src/resources/user/user.controllers.js
+++ src/resources/user/user.controllers.js
@@ -1,17 +1,21 @@
-// after models get started on controller
-import { User } from './user.models'
-import mongoose from 'mongoose'
-import { connect } from '../../utils/db'
+// // after models get started on controller
+// import { User } from './user.models'
+// import mongoose from 'mongoose'
+// // do I really need this uri? If so, mI need to require .env variables
+// require('dotenv').config({ path: '../../../.env' })
 
-const run = async () => {
-  await connect('mongodb://localhost:27017/api')
-  const user = await User.create({
-    username: 'jnnnle',
-    email: 'jennifer@email.com',
-    password: 'password',
-    location: 'Oakland'
-  })
-  console.log(user)
-}
+// let uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@doggydate-biwhc.gcp.mongodb.net/test?retryWrites=true&w=majority`
 
-run()
+// const run = async () => {
+//   //the code on line 11 was created after a series of error messages, at this point, I don't know what is going on and need to take a step back
+//   await mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true })
+//   const user = await User.create({
+//     username: 'jnnnle',
+//     email: 'jennifer@email.com',
+//     password: 'password',
+//     location: 'Oakland'
+//   })
+//   console.log(User.find({}).exec())
+// }
+
+// run()
