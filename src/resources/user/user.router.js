d9eb78acd38112e19b9ab11b90349a97628f8692
JnnnLe <jnnnle.sf@gmail.com>
2019-10-24 15:09:03 -0700

    Delete dummy controller

--- src/resources/user/user.router.js
+++ src/resources/user/user.router.js
@@ -1,21 +1,37 @@
 import { Router } from 'express'
+import { User } from './user.models'
+import mongoose from 'mongoose'
+require('dotenv').config()
 
 const controller = (req, res) => {
   res.send({ message: 'The way that you habitually think determines the way that you habitually feel.' })
 }
 const router = Router()
 
+let uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@doggydate-biwhc.gcp.mongodb.net/test?retryWrites=true&w=majority`
+
+const createOne = async (req, res) => {
+  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  const user =  await User.create({
+    username: 'max',
+    email: 'max@email.com',
+    password: 'password',
+    location: 'Oakland'
+  })
+  res.send(user)
+}
+
 router
   // /api/...
   .route('/')
   .get(controller)
-  .post(controller)
+  .post(createOne)
 
 router
   // /api/:id...
   .route('/:id')
-  .put(controller)
-  .delete(controller)
-  .get(controller)
+  .put()
+  .delete()
+  .get()
 
 export default router
