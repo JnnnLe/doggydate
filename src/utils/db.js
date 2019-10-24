a69c99356fe2a56545893e3ab8f1e1308346955d
JnnnLe <jnnnle.sf@gmail.com>
2019-10-24 15:10:21 -0700

    Edit syntax in console log

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,5 +4,5 @@ require('dotenv').config({ path: '../../.env' })
 let uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@doggydate-biwhc.gcp.mongodb.net/test?retryWrites=true&w=majority`
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
-  err ? console.log(err) : console.log('Success, connected to Mongo database :).')
+  err ? console.log(err) : console.log('Success, connected to Mongo database :)')
 })
