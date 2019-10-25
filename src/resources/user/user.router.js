2439b7974d8272641db07fc177d141a502140249
JnnnLe <jnnnle.sf@gmail.com>
2019-10-25 08:53:36 -0700

    Move envocation of Router

--- src/resources/user/user.router.js
+++ src/resources/user/user.router.js
@@ -3,15 +3,16 @@ import { User } from './user.model'
 import mongoose from 'mongoose'
 require('dotenv').config()
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@doggydate-biwhc.gcp.mongodb.net/api?retryWrites=true&w=majority`
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const router = Router()
 const controller = (req, res) => {
   res.send({ message: 'The way that you habitually think determines the way that you habitually feel.' })
 }
-const router = Router()
 
-let uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@doggydate-biwhc.gcp.mongodb.net/api?retryWrites=true&w=majority`
 
 const createOne = async (req, res) => {
-  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   const user =  await User.create({
     username: req.body.username,
     email: req.body.email,
