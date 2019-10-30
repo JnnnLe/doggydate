2db5e0f3ba163a8f1b9ee9a8ca331948e2f1d094
JnnnLe <jnnnle.sf@gmail.com>
2019-10-30 13:49:12 -0700

    Add user info to form using hooks

--- client/src/Components/AccountInfo.js
+++ client/src/Components/AccountInfo.js
@@ -1,34 +1,72 @@
 import React from 'react'
 
-class AccountInfo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: 'Jennifer',
-      email: '',
-      password: '',
-      location: '',
-      petName: '',
-      petImg: '',
-      breed: '',
-      age: '',
-      weight: '',
-      fixed: '',
-      vaccinated: '',
-      energyLevel: '',
-      heatSensitivity: '',
-      waterCompatibility: '',
-      Availability: ''
-    }
+function AccountInfo () {
+  const [state, setState] = React.useState({
+    username: '',
+    email: '',
+    password: '',
+    location: ''
+    // petName: '',
+    // petImg: '',
+    // breed: '',
+    // age: '',
+    // weight: '',
+    // fixed: '',
+    // vaccinated: '',
+    // energyLevel: '',
+    // heatSensitivity: '',
+    // waterCompatibility: '',
+    // Availability: ''
+  })
+
+  function handleChange(evt) {
+    const value = evt.target.value;
+    setState({
+      ...state,
+      [evt.target.name]: value
+    });
   }
 
-  render() {
-    return (
-      <div>
-        <h3>Hello Jennifer</h3>
-      </div>
-    )
-  }
+  return (
+    <form>
+      <label>
+        Username
+        <input
+          type="text"
+          name="username"
+          value={state.username}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Email
+        <input
+          type="text"
+          name="email"
+          value={state.email}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="text"
+          name="password"
+          value={state.password}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Location
+        <input
+          type="text"
+          name="location"
+          value={state.location}
+          onChange={handleChange}
+        />
+      </label>
+    </form>
+  );
 }
 
 export default AccountInfo
