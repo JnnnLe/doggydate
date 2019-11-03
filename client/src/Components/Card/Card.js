fbb58b4783e32f52bd02459d2fbfd80c4bd052df
JnnnLe <jnnnle.sf@gmail.com>
2019-11-03 15:20:10 -0800

    Pass state as props to Card

--- client/src/Components/Card/Card.js
+++ client/src/Components/Card/Card.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import './Card.css'
 import Avatar from '@material-ui/core/Avatar';
 
-const Card = () => {
+const Card = (props) => {
+
   return (
     <div>
       <Avatar className="avatar" alt="Dog" src="https://www.petmd.com/sites/default/files/Acute-Dog-Diarrhea-47066074.jpg" />
+      <h1>{props.petName}</h1>
     </div>
   )
 }
--- client/src/Views/Feed.js
+++ client/src/Views/Feed.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import Card from '../Components/Card/Card'
 
+const Feed = () => {
+  const [ allPets, setAllPets] = useState([])
 
-function Feed () {
+  const getAllPets = async () => {
+    let pets = await axios.get('http://localhost:3000/api/user/pet')
+
+    if (pets.data == 'You have no pets.') {
+      return pets.data
+    }
+    setAllPets(pets.data)
+  } 
+
+  const renderPets = () => {
+    return allPets.map(dog => <Card {...dog} /> )
+  }
+
   return (
     <div>
       <h1>Welcome to the Feed!</h1>
-      <Card />
+      {renderPets()}
+      <button onClick={getAllPets}>Hit me!</button>
     </div>
   )
 }
