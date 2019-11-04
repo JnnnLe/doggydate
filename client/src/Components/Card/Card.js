faffb1de25335bd4670abaf7ca1bbc5a6ebba115
JnnnLe <jnnnle.sf@gmail.com>
2019-11-03 16:42:05 -0800

    Some style of Card

--- client/src/Components/Card/Card.js
+++ client/src/Components/Card/Card.js
@@ -6,15 +6,15 @@ const Card = (props) => {
 
   return (
     <div>
-      <Avatar className="avatar" alt="Dog" src="{props.imgURL}" />
+      <Avatar className="avatar" alt="Dog" src={props.imgURL} />
       <div className="info">
         <h2>{props.petName} - {props.age} YO</h2> 
         <div><h3>{props.breed} - {props.weight} lbs.</h3></div>
         <div><h3>{props.vaccinated ? <div>Vaccinated: ✅</div>  : <div>Vaccinated: ❌</div>} </h3></div>
         <div><h3>{props.fixed ? <div>Fixed: ✅</div>  : <div>Fixed: ❌</div>} </h3></div>
-        <div><h3>{props.energyLevel ? <div>Energy Level: ✅</div>  : <div>Energy Level: ❌</div>} </h3></div>
-        <div><h3>{props.heatSensitivity ? <div>Heat Sensitivity: ✅</div>  : <div>Heat Sensitivity: ❌</div>} </h3></div>
-        <div><h3>{props.waterCompatiability ? <div>Water Compatiability: ✅</div>  : <div>Water Compatiability: ❌</div>} </h3></div>
+        <div><h3>Energy Level: {props.energyLevel} </h3></div>
+        <div><h3>{props.heatSensitivity ? <div>Heat Sensitive: ✅</div>  : <div>Heat Sensitivity: ❌</div>} </h3></div>
+        <div><h3>{props.waterCompatiability ? <div>Water Compatiable: ✅</div>  : <div>Water Compatiability: ❌</div>} </h3></div>
       </div>
     </div>
   )
