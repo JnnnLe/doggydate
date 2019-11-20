3c39c69e96cfb748fd698fc28d306d17a7f81f8c
JnnnLe <jnnnle.sf@gmail.com>
2019-11-20 09:53:54 -0800

    Get user email, checkJwt & pass to the server

--- client/src/Components/Favorites/Favorites.js
+++ client/src/Components/Favorites/Favorites.js
@@ -5,28 +5,23 @@ import axios from 'axios'
 
 const Favorites = () => {
   const { getTokenSilently, user } = useAuth0()
-
-  const [ loggedUser, setUser ] = useState(user)
   const [favorites, setFavorites ] = useState([])
-
-  const getFavorites = async () => {
-    const token = await getTokenSilently()
-    // console.log('FE, email:', email)
-    // if (user) {
-
-      await axios.post('/favorites', {
-        params: { user: loggedUser },
+  const getFavorites =  async () => {
+    const token =  await getTokenSilently()
+       await axios.get('/favorites', {
+        params: {
+          email: user.email
+        },
         headers: {
           Authorization: `Bearer ${token}`
         }
       })
       .then(response => console.log('Favorites FE:', response))
-    // }
   }
 
   useEffect(() => {
     getFavorites()
-  }, [])
+  }, [user])
 
 
     return (
