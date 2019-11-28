1d05337abc3765765470a0d01822f31746c2783c
JnnnLe <jnnnle.sf@gmail.com>
2019-11-28 12:32:52 -0500

    Delete usused dependecies and clean up css

--- client/src/Components/LargeCard/LargeCard.js
+++ client/src/Components/LargeCard/LargeCard.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import './LargeCard.css'
-import Avatar from '@material-ui/core/Avatar';
 import axios from 'axios'
 import { useAuth0 } from '../../react-auth0-spa'
-import { FaDog, FaHome } from "react-icons/fa"
+import { FaDog } from "react-icons/fa"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
@@ -27,27 +26,31 @@ const LargeCard = (props) => {
     })
     .catch(err => console.log(`Error in Error occurred when adding ${petObject.name} to favorites:`, err))
   }
-  console.log(props)
+
   let photos = props.photos // some animals do not have photos
+  
   return (
     <div className="large__card">
       <Carousel>
       {
-         photos.map(photo => (
+        photos.map(photo => (
           <div className="large__card__photo-img">
-          <img src={photo.large} height="300px" width="220px"/>
+            <img src={photo.large} height="300px" width="220px" alt={props.name} key={props.id} />
           </div>
         ))
       }
       </Carousel>
-      <div classname=".large__card__details">
+      <div className=".large__card__details">
         <h4 className="large__card__details-name">{props.name}</h4>
         <p className="large__card__details-location"><b>{props.contact.address.city}, {props.contact.address.state}</b></p>
-        <div className="large__card__bio">Hi, I'm {props.name} and I am a {props.age} {props.breeds.primary}. Adopt me below.
-        <div clasName="large__card__adopt">
-          <a className="adopt" id={props.id} href={props.url} target="_blank"> <FaDog /></a>
+
+        <div className="large__card__bio">
+          Hi, I'm {props.name} and I am a {props.age} {props.breeds.primary}. Adopt me below.
+
+          <div className="large__card__adopt">
+            <a className="adopt" id={props.id} href={props.url} target="_blank"> <FaDog /></a>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   )
