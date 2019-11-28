a440cb79c0b2843b7560cc4dea86430aaf75e5dd
JnnnLe <jnnnle.sf@gmail.com>
2019-11-28 12:16:34 -0500

    Delete unused dependencies and reorganized css

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,38 +1,32 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { BrowserRouter as Router, Link, Redirect } from 'react-router-dom'
+import React from 'react'
+import { Redirect } from 'react-router-dom'
 import './Login.css'
 import Button from '@material-ui/core/Button'
-import { useAuth0 } from "../../react-auth0-spa";
-import Feed from '../Feed/Feed'
+import { useAuth0 } from "../../react-auth0-spa"
 
 const Login = () => {
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
-  const [ email, setEmail ] = useState('')
-  const [ password, setPassword ] = useState('')
+  const { isAuthenticated, loginWithRedirect } = useAuth0()
   
   return (
     <div>
       {isAuthenticated ? 
         <Redirect from='/' to='/feed' /> : 
-        <div className="login">
+          <div className="login">
           <div className="loginLeft" />
           <div className="loginRight">
             <h1 className="loginHeader">Welcome to doggydate</h1>
-            <br />
-            
-              <Button
+            <br />   
+            <Button
               variant="contained"
               className="loginButton"
-              onClick={() =>
-                loginWithRedirect({})
-                }
-              >
-                Let's begin
-              </Button>
+              onClick={() => loginWithRedirect({})} 
+            >
+              Let's begin
+            </Button>
           </div>
         </div>}
       </div>
   )
 }
+
 export default Login
